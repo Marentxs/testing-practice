7f8995d360a458080b66e2421c7fd63f16cdc9ed
Julian <111262742+Marentxs@users.noreply.github.com>
2026-02-02 16:57:21 -0500

    add test and code for calculator object

--- all.test.js
+++ all.test.js
@@ -1,6 +1,7 @@
 import sum from "./sum.js";
 import capitalize from "./capitalize.js";
 import reverseString from "./reverseString.js";
+import calculator from "./calculator.js";
 
 test("capitalize hello", () => {
   expect(capitalize("hello")).toBe("Hello");
@@ -12,4 +13,20 @@ test("adds 1 + 2 to equal 3", () => {
 
 test("reverse hello", () => {
   expect(reverseString("hello")).toBe("olleh");
+});
+
+test("adds 6 + 4 to equal 10", () => {
+  expect(calculator.sum(6, 4)).toBe(10);
+});
+
+test("substract 6 - 4 to equal 2", () => {
+  expect(calculator.substract(6, 4)).toBe(2);
+});
+
+test("multiply 6 * 4 to equal 24", () => {
+  expect(calculator.multiply(6, 4)).toBe(24);
+});
+
+test("divide 6 / 4 to equal 1.5", () => {
+  expect(calculator.divide(6, 4)).toBe(1.5);
 });
