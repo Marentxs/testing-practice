e9f0e2507e0482336695e661d9277b9ad2b0f691
Julian <111262742+Marentxs@users.noreply.github.com>
2026-02-02 17:22:54 -0500

    add test and code for analyzeArray

--- all.test.js
+++ all.test.js
@@ -2,6 +2,7 @@ import sum from "./sum.js";
 import capitalize from "./capitalize.js";
 import reverseString from "./reverseString.js";
 import calculator from "./calculator.js";
+import analyzeArray from "./analyze.js";
 
 test("capitalize hello", () => {
   expect(capitalize("hello")).toBe("Hello");
@@ -29,4 +30,13 @@ test("multiply 6 * 4 to equal 24", () => {
 
 test("divide 6 / 4 to equal 1.5", () => {
   expect(calculator.divide(6, 4)).toBe(1.5);
+});
+
+test("analyze the array and return correct analysis", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
---
+++ analyze.js
@@ -0,0 +1,13 @@
+export default function analyzeArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return {
+    average: sum / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+}
